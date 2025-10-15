b14a02c7a0d98d729c6c0a138c57189dbd80e182
Tebogo Adam Mphahlele <tebogomphahlele12345@gmail.com>
2025-10-15 10:27:04 +0200

    added environment.dev.ts for the firebase credentials

---
+++ fleet-manager/src/environments/environment.dev.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  //replace with origanal keys
+  firebase: {
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  }
+};
